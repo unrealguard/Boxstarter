5e8c7ad818b6912adabe4915b8bc714864024b40
unrealguard <bj7857@gmail.com>
2015-09-08 14:34:47 -0400

    Create Web.ps1

---
+++ Web.ps1
@@ -0,0 +1,16 @@
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+
+cinst tortoisesvn
+cinst nuget.commandline
+cinst nodejs
+
+
+cinst visualstudiocode
+cinst vim
+
+
+cinst firefox
+cinst chrome
+
+
+cinst IIS-WebServerRole -source windowsfeatures
